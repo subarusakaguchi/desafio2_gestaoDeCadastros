f3b9faca0732deece6c4480c9737bff8f095b2c2
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-07-20 21:40:38 -0300

    Tentativas de menu com dados do cliente

--- script/script.js
+++ script/script.js
@@ -9,6 +9,12 @@ onload = function () {
             id[i] = Number(idListString[i])
         }
     }
+
+    if (id.length != 0) {
+        id.map(function (idItem) {
+            attClient(JSON.parse(localStorage.getItem(`client_${idItem}`)))
+        })
+    }
 }
 
 const clientRegister = document.getElementById('btnClient').addEventListener('click', function add(e) {
@@ -65,12 +71,42 @@ function createClient(id, name, cpf, email, cep, number, complement) {
 function attClient(client) {
     let resClientContainer = document.getElementById('resClientContainer')
     resClientContainer.classList.add('menu')
-    
     let resClient = document.getElementById('resClient')
     let item = document.createElement('li')
+    let ul = document.createElement('ul')
+    item.setAttribute('id', `client_${client.id}`)
     item.innerHTML = `${client.nome}`
+    ul.setAttribute('id', `infoClient_${client.id}`)
+    ul.classList.add('hidden')
+    addData(ul, client)
+
+
+    item.addEventListener('click', toggleClient)
+    item.appendChild(ul)
     resClient.appendChild(item)
 }
+
+const addData = function (element, client) {
+    Object.keys(client).forEach(function(key) {
+        let li = new createLi(`${key.toUpperCase()}: ${client[key]}`)
+        element.appendChild(li)
+    })
+}
+
+function createLi(texto) {
+    const li = document.createElement('li')
+    li.classList.add('showInfo')
+    li.innerHTML = `${texto}`
+    return li
+}
+
+// Função para aparecer os dados dos clientes
+const toggleClient = function () {
+    const id = this.id
+    let div = document.getElementById(`${id}`)
+    div.classList.toggle('active')
+}
+
 
 function clearAll() {
     localStorage.clear()
